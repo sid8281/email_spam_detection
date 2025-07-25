5e7ff659d4a2afd9c8a04d540b5cb66b9a688fb9
sid8281 <sidshinde2901@gmail.com>
2025-07-25 19:11:12 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -18,7 +18,8 @@ y = df['label']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # TF-IDF Vectorization
-vectorizer = TfidfVectorizer(stop_words='english')
+vectorizer = TfidfVectorizer(stop_words='english', max_df=0.9, ngram_range=(1, 3))
+
 X_train_tfidf = vectorizer.fit_transform(X_train)
 X_test_tfidf = vectorizer.transform(X_test)
 
